c9c5c78ad62dcb994ade55b78042c76d2ca9961f
albertomopa28 <albertomochonparedes@gmail.com>
2025-01-03 17:07:15 +0100

    ehn

--- Otros_trabajos/Cohete_Marte/Energia.py
+++ Otros_trabajos/Cohete_Marte/Energia.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Nombre del archivo
-archivo = "c:/Users/Equipo/Documents/GitHub/Simulaciones-y-Documentos-de-mi-Trayectoria-en-F-sica/Otros trabajos/Cohete_Marte/Energia.dat"
+archivo = "Energia.dat"
 
 # Leer datos del archivo
 data = np.loadtxt(archivo)
